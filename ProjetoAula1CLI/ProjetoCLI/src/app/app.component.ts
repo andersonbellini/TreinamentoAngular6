d6a57f1f34a1495a13f69cbac4f175f2363e5870
anderson.bellini <anderson.bellini@anac.gov.br>
2018-09-11 19:27:25 -0300

    Usando Diretivas ngFor, ngIF, ngSwitch, ngClass etc

--- ProjetoAula1CLI/ProjetoCLI/src/app/app.component.ts
+++ ProjetoAula1CLI/ProjetoCLI/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Pessoa } from './pessoa';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,22 +9,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PIC Angular';
 
-  pessoas: any[] = [
-    {
-      "nome":'Anderson Bellini',
-      "idade": 30
-    },
-    {
-      "nome":"Odair Viol",
-      "idade": 32
-    },
-    {
-      "nome":"Roberto Bob",
-      "idade": 38
-    }
-    ,{
-      "nome":"João Silva",
-      "idade": 40
-    }
+  pessoas: Pessoa[] = [
+    new Pessoa("Anderson Bellini", 30),
+    new Pessoa("Odair Viol", 32),
+    new Pessoa("Roberto Bob", 38),
+    new Pessoa("João Silva", 40)
   ]
+
 }
+
+// // Foi removido para usar classe e objetos
+// pessoas: any[] = [
+//   {
+//     "nome":'Anderson Bellini',
+//     "idade": 30
+//   },
+//   {
+//     "nome":"Odair Viol",
+//     "idade": 32
+//   },
+//   {
+//     "nome":"Roberto Bob",
+//     "idade": 38
+//   }
+//   ,{
+//     "nome":"João Silva",
+//     "idade": 40
+//   }
+// ]
---
+++ ProjetoAula1CLI/ProjetoCLI/src/app/pessoa.ts
@@ -0,0 +1,9 @@
+export class Pessoa {
+  constructor(
+      public nome: string,
+      public idade: any)
+  {
+  }
+
+
+}

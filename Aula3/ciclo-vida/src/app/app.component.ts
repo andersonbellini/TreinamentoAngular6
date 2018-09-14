7d12ffe7b6350cf3ec8e0ab6c79fdc033f8b55cb
anderson.bellini <anderson.bellini@anac.gov.br>
2018-09-14 14:49:34 -0300

    Update Components

--- Aula3/ciclo-vida/src/app/app.component.ts
+++ Aula3/ciclo-vida/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
+
   title = 'ciclo-vida';
 
   valorComponentePai: number = 10;

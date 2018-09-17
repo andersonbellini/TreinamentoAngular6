272ae021cb47655d801342ba57552af09a46c49c
anderson.bellini <anderson.bellini@anac.gov.br>
2018-09-17 17:37:47 -0300

    Criação da estrutura para inicio do projeto

--- Aula4/cast-anuncios/src/app/app.module.ts
+++ Aula4/cast-anuncios/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnuncioConsultaComponent } from './components/anuncios/anuncio-consulta/anuncio-consulta.component';
+import { AnuncioCadastroComponent } from './components/anuncios/anuncio-cadastro/anuncio-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnuncioConsultaComponent,
+    AnuncioCadastroComponent
   ],
   imports: [
     BrowserModule

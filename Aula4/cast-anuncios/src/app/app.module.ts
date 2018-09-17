fb947105e45554d46349e8e0a20cfed3141cb08e
anderson.bellini <anderson.bellini@anac.gov.br>
2018-09-17 17:41:17 -0300

    Estrutura da Aplicação Header, Footer e home

--- Aula4/cast-anuncios/src/app/app.module.ts
+++ Aula4/cast-anuncios/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AnuncioConsultaComponent } from './components/anuncios/anuncio-consulta/anuncio-consulta.component';
 import { AnuncioCadastroComponent } from './components/anuncios/anuncio-cadastro/anuncio-cadastro.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnuncioConsultaComponent,
-    AnuncioCadastroComponent
+    AnuncioCadastroComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule

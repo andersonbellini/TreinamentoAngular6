fcec053fff4b40fa5f9c77a8b39888f490880fb9
anderson.bellini <anderson.bellini@anac.gov.br>
2018-09-17 18:57:36 -0300

    Correção aula4

--- Aula4/cast-anuncios/src/app/app.module.ts
+++ Aula4/cast-anuncios/src/app/app.module.ts
@@ -4,21 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AnuncioConsultaComponent } from './components/anuncios/anuncio-consulta/anuncio-consulta.component';
 import { AnuncioCadastroComponent } from './components/anuncios/anuncio-cadastro/anuncio-cadastro.component';
-import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnuncioConsultaComponent,
     AnuncioCadastroComponent,
-    HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Aula4/cast-anuncios/src/app/services/tipo-anuncio.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TipoAnuncioService {
+
+  constructor() { }
+
+  findAll(){
+    Console.log("aqui");
+  }
+}

d053049d247cda66fec63d55741eb4889ec100f2
anderson.bellini <anderson.bellini@anac.gov.br>
2018-09-18 18:37:40 -0300

    update anuncio

--- Aula4/cast-anuncios/src/app/components/anuncios/anuncio-cadastro/anuncio-cadastro.component.ts
+++ Aula4/cast-anuncios/src/app/components/anuncios/anuncio-cadastro/anuncio-cadastro.component.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { TipoAnuncioService } from './../../../services/tipo-anuncio.service';
 import { Component, OnInit } from '@angular/core';
 import { tipoAnuncio } from '../../../models/tipo-anuncio.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-anuncio-cadastro',
@@ -10,12 +11,26 @@ import { tipoAnuncio } from '../../../models/tipo-anuncio.model';
 })
 export class AnuncioCadastroComponent implements OnInit {
 
+  formulario: FormGroup;
+
   tiposAnuncio: Observable<tipoAnuncio[]>;
 
-  constructor(private tipoAnuncioService: TipoAnuncioService) { }
+  constructor(private tipoAnuncioService: TipoAnuncioService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      tipo: [null, Validators.required],
+      nome: [null, Validators.required],
+      descricao: [null, Validators.required],
+      valor: [null, Validators.required],
+      contato: [null, Validators.required]
+    })
     this.tiposAnuncio = this.tipoAnuncioService.findAll();
+  }
+
+  public salvar(): void {
+    console.log(this.formulario);
+    console.log(this.formulario.value);
   }
 
 }
---
+++ Aula4/cast-anuncios/src/app/models/anuncio.model.ts
@@ -0,0 +1,14 @@
+export  class anuncio {
+
+  constructor
+  (
+    public id: number,
+    public contato: string,
+    public descricao: string,
+    public nome: string,
+    public tipo: number,
+    public valor: string,
+    ){}
+
+
+}

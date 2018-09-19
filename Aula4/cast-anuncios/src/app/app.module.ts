6ad87b8ac45959781d38e41c3b879c8a35e2ff75
anderson.bellini <anderson.bellini@anac.gov.br>
2018-09-19 19:12:36 -0300

    Adicionado Mascara

--- Aula4/cast-anuncios/src/app/app.module.ts
+++ Aula4/cast-anuncios/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
 
 import ErrorHttpInterceptor from './utils/interceptors/error-http-interceptors';
 import { CurrencyMaskModule } from "ng2-currency-mask";
+import {NgxMaskModule} from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { CurrencyMaskModule } from "ng2-currency-mask";
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    CurrencyMaskModule
+    CurrencyMaskModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
